7ef42b1157f096b4b11b98297f5ca1b87fd34b68
ahmet-parlak <ahmet--parlak@outlook.com>
2023-12-31 16:34:58 +0300

    Theme Integration and Routing Update
    - Integrated the project theme and segmented it.
    - Adapted the page router and controller to the theme's pages.

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,3 +1,16 @@
 exports.getIndexPage = (req, res) => {
-  res.status(200).render('index');
+  const currentPage = 'index';
+  res.status(200).render('index', { currentPage });
+};
+exports.getAboutPage = (req, res) => {
+  const currentPage = 'about';
+  res.status(200).render('about', { currentPage });
+};
+exports.getFurnituresPage = (req, res) => {
+  const currentPage = 'furnitures';
+  res.status(200).render('furnitures', { currentPage });
+};
+exports.getContactPage = (req, res) => {
+  const currentPage = 'contact';
+  res.status(200).render('contact', { currentPage });
 };
--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -4,5 +4,8 @@ const pageController = require('../controllers/pageController');
 const router = express.Router();
 
 router.get('/', pageController.getIndexPage);
+router.get('/about', pageController.getAboutPage);
+router.get('/furnitures', pageController.getFurnituresPage);
+router.get('/contact', pageController.getContactPage);
 
 module.exports = router;
